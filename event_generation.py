ff0895f64adf1451f866e546865e630a9228a119
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2018-04-16 18:44:15 +0200

    Add event_generation python script using basf2
    Furthermore add a corresponding '.dec' file describing a sample decay.

---
+++ event_generation.py
@@ -0,0 +1,18 @@
+import basf2
+
+from modularAnalysis import outputMdst
+from reconstruction import add_reconstruction
+from simulation import add_simulation
+
+path = basf2.create_path()
+
+# Add modules to the root basf2 path; Parameters can be retrieved using basf2.print_params(`moduleVar`)
+setter = path.add_module("EventInfoSetter", evtNumList=[100000])
+generator = path.add_module("EvtGenInput", userDECFile='b2_mc_event.dec')
+
+add_simulation(path)
+add_reconstruction(path)
+
+outputMdst('b2_mc_events.mdst.root', path=path)
+
+basf2.process(path)
