a6b699111ea3486a0d7948abfc970fd9be1bace4
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2018-04-17 18:44:01 +0200

    Add event_metrics python evaluation script
    Introduce a python script for evaluating the preprocessed data using
    root's flavor of pandas for drawing binned likelihoods histograms.

---
+++ event_metrics.py
@@ -0,0 +1,29 @@
+import root_pandas as rpd
+import scipy
+import scipy.stats
+import numpy as np
+import matplotlib.pyplot as plt
+
+nbins = 50
+log_cutoff = -5
+
+particles_of_interest = ['K-', 'pi+']   # Pay extra attention to use the same order as for the preprocessing step otherwise the epsilon_PID matrix will be off
+detector_list = ['svd', 'cdc', 'top', 'arich', 'ecl', 'klm']
+detector_list = ['arich']
+particleID_list = ['kaonID', 'pionID']
+
+data = rpd.read_root('sample.root')
+#print(data.describe())
+
+for d in detector_list:
+    plt.suptitle('Binned likelihood for the %s detector signal'%(d))
+    for i, p in enumerate(particles_of_interest):
+        for i_2, p_2 in enumerate(particles_of_interest):
+            plt.subplot(len(particles_of_interest), len(particles_of_interest), i*len(particles_of_interest)+i_2+1)
+            plt.title('%s as %s'%(p, p_2))
+            column = 'pidLogLikelihoodValueExpert_' + d + 'Detector_daughter' + str(i) + 'asDaughter' + str(i_2)
+            print(data[column].describe())
+            data[data[column] > log_cutoff][column].apply(lambda x: np.exp(x)).hist(bins=nbins)
+
+    plt.show()
+
