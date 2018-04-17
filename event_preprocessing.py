83d978e961dfe23b4d0d9ed55f052e083af61156
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2018-04-17 16:43:53 +0200

    event_preprocessing.py: Add likelihood information
    Add pidLogLikelihoodValueExpert_for each particle of the decay and every
    detector in the experiment to the output. Use aliases for simplicity.

--- event_preprocessing.py
+++ event_preprocessing.py
@@ -1,23 +1,37 @@
 import basf2
 import modularAnalysis
 import pdg
+import variables
 
 path = basf2.create_path()
+rootVars = []
+
+# Specify the decay of interest
+vertex_parent = 'D0'
+particles_of_interest = ['K-', 'pi+']
+rootVars += ['daughter(0, kaonID)', 'daughter(1, pionID)']
+decay = vertex_parent + ' -> ' + ' '.join(particles_of_interest)
+
+detector_list = ['svd', 'cdc', 'top', 'arich', 'ecl', 'klm']
+
+var_list = []
+for i, p in enumerate(particles_of_interest):
+    for d in detector_list:
+        var_list += ['pidLogLikelihoodValueExpert_' + 'daughter' + str(i) + '_' + d + 'Detector']
+        variables.variables.addAlias(var_list[-1], 'daughter(' + str(i) + ', ' + 'pidLogLikelihoodValueExpert(' + str(pdg.from_name('K-')) + ', ' + str(d) + ')' + ')')
+
+variables.variables.addCollection('pidLogLikelihoodValueExpertDaughtersByDetector', variables.std_vector(*var_list)) 
+rootVars += ['pidLogLikelihoodValueExpertDaughtersByDetector']
 
 modularAnalysis.inputMdstList("default", ['sample.mdst.root'], path=path)
-modularAnalysis.fillParticleLists([('K+', ''), ('pi+', ''), ('mu+', '')], path=path)
-modularAnalysis.reconstructDecay('D0 -> K- pi+', '', path=path)
-modularAnalysis.fitVertex('D0', 0., path=path)
-modularAnalysis.matchMCTruth('D0', path=path)
-
-# Specify the particle of interest by Particle Data Group Code (pdgCode)
-pdg_code = pdg.from_name('K-')
-# Specify the detector of interest by name, see the 'parseDetectors' function in the C++ code
-detector = 'top'
+modularAnalysis.fillParticleLists([(p, '') for p in particles_of_interest], path=path)
+modularAnalysis.reconstructDecay(decay, '', path=path)
+modularAnalysis.fitVertex(vertex_parent, 0., path=path)
+modularAnalysis.matchMCTruth(vertex_parent, path=path)
 
 # Export variables of the analysis to NTuple root file
 # Inspect the value using modularAnalysis.printVariableValues('K+', `varName(varArg)`, path=path)
-modularAnalysis.variablesToNTuple('D0', ['Mbc', 'deltaE', 'M', 'p', 'E', 'useCMSFrame(p)', 'useCMSFrame(E)', 'daughter(0, kaonID)', 'daughter(1, pionID)', 'isSignal', 'mcErrors'], filename='sample.root', path=path)
-modularAnalysis.variablesToNTuple('K+', 'pidLogLikelihoodValueExpert(' + str(pdg_code) + ', ' + detector + ')', filename='K+_sample.root', path=path)
+rootVars += ['isSignal', 'mcErrors']
+modularAnalysis.variablesToNTuple(vertex_parent, rootVars, filename='sample.root', path=path)
 
 basf2.process(path)
