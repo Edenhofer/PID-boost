38e5ef2c5736171f3da4475190376b1c057ae21b
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2018-04-17 16:58:38 +0200

    event_preprocessing.py: Add particleID information
    Add a list of particleID values to each daughter particle to the output.

--- event_preprocessing.py
+++ event_preprocessing.py
@@ -9,13 +9,16 @@ rootVars = []
 # Specify the decay of interest
 vertex_parent = 'D0'
 particles_of_interest = ['K-', 'pi+']
-rootVars += ['daughter(0, kaonID)', 'daughter(1, pionID)']
 decay = vertex_parent + ' -> ' + ' '.join(particles_of_interest)
 
 detector_list = ['svd', 'cdc', 'top', 'arich', 'ecl', 'klm']
+particleID_list = ['kaonID', 'pionID']
 
 var_list = []
 for i, p in enumerate(particles_of_interest):
+    for n in particleID_list:
+        variables.variables.addAlias('daughter' + str(i) + '_' + n, 'daughter(' + str(i) + ', ' + n + ')')
+        rootVars += ['daughter' + str(i) + '_' + n]
     for d in detector_list:
         var_list += ['pidLogLikelihoodValueExpert_' + 'daughter' + str(i) + '_' + d + 'Detector']
         variables.variables.addAlias(var_list[-1], 'daughter(' + str(i) + ', ' + 'pidLogLikelihoodValueExpert(' + str(pdg.from_name('K-')) + ', ' + str(d) + ')' + ')')
