7c2b95f856431fc28242eb117243505980c5adcf
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2018-04-20 17:36:31 +0200

    event_preprocessing.py: Strip redundant type
    Remove redundant statement of the variable type in the name of the
    variable.

--- event_preprocessing.py
+++ event_preprocessing.py
@@ -5,8 +5,8 @@ import pdg
 path = basf2.create_path()
 
 # Base definitions of stable particles and detector data
-particle_list = ['K+', 'pi+', 'e+', 'mu+', 'p+', 'deuteron']
-detector_list = ['svd', 'cdc', 'top', 'arich', 'ecl', 'klm']
+particle = ['K+', 'pi+', 'e+', 'mu+', 'p+', 'deuteron']
+detectors = ['svd', 'cdc', 'top', 'arich', 'ecl', 'klm']
 
 # Default list of variables which should be exported to the root file
 root_vars = ['isSignal', 'mcErrors', 'mcPDG']
@@ -14,12 +14,12 @@ root_vars += ['kaonID', 'pionID','electronID',  'muonID', 'protonID', 'deuteronI
 
 # Import mdst file and fill particle list without applying any cuts
 modularAnalysis.inputMdstList("default", ['sample.mdst.root'], path=path)
-modularAnalysis.fillParticleLists([(p, '') for p in particle_list], path=path)
+modularAnalysis.fillParticleLists([(p, '') for p in particle], path=path)
 
-for p in particle_list:
+for p in particle:
     child_vars = []
-    for d in detector_list:
-        for p_2 in particle_list:
+    for d in detectors:
+        for p_2 in particle:
             child_vars += ['pidLogLikelihoodValueExpert(' + str(pdg.from_name(p_2)) + ', ' + str(d) + ')']
 
     # Export variables of the analysis to NTuple root file
