Edenhofer/PID-boost

commit 1e01f30a9d8520249ea45c054ed3ef98f3626c54

event_metrics.py run_pid: Be specific about column

event_metrics.py
@@ -140,9 +140,8 @@ if args.run_pid:
 
     particles_of_interest = args.particles_of_interest
 
-    data.plot_stats_by_particle(data.stats(), particles_of_interest=particles_of_interest, savefig_prefix='Particle ID Approach: ')
-
     c = data.add_isMax_column(ParticleFrame.particleIDs) if exclusive_cut else ParticleFrame.particleIDs
+    data.plot_stats_by_particle(data.stats(cutting_columns=c), particles_of_interest=particles_of_interest, savefig_prefix='Particle ID Approach: ')
     epsilonPIDs = data.epsilonPID_matrix(cutting_columns=c, cut=cut)
     if exclusive_cut:
         drawing_title = r'Heatmap of $\epsilon_{PID}$ Matrix for an exclusive Cut'

commit b70f90f75e18bd06f6e606d11c6bf27349480f31

event_metrics.py pid and pidProbability: isMax
Calculate isMax column after plotting the statistics.
For pid this bug was introduced in 1e01f30a9d.

event_metrics.py
@@ -142,8 +142,9 @@ if args.run_pid:
 
     particles_of_interest = args.particles_of_interest
 
-    c = data.add_isMax_column(ParticleFrame.particleIDs) if exclusive_cut else ParticleFrame.particleIDs
+    c = ParticleFrame.particleIDs
     data.plot_stats_by_particle(data.stats(cutting_columns=c), particles_of_interest=particles_of_interest, savefig_prefix='Particle ID Approach: ')
+    c = data.add_isMax_column(c) if exclusive_cut else c
     epsilonPIDs = data.epsilonPID_matrix(cutting_columns=c, cut=cut)
     if exclusive_cut:
         drawing_title = r'Heatmap of $\epsilon_{PID}$ Matrix for an exclusive Cut'
@@ -160,8 +161,8 @@ if args.run_pidProbability:
     detector = 'all'
 
     c = {p: 'pidProbabilityExpert__bo' + lib.basf2_Code(p) + '__cm__sp' + detector + '__bc' for p in ParticleFrame.particles}
-    c = data.add_isMax_column(c) if exclusive_cut else c
     data.plot_stats_by_particle(data.stats(cutting_columns=c), particles_of_interest=particles_of_interest, savefig_prefix='Particle ID Approach: ')
+    c = data.add_isMax_column(c) if exclusive_cut else c
     epsilonPIDs = data.epsilonPID_matrix(cutting_columns=c, cut=cut)
     if exclusive_cut:
         drawing_title = r'Heatmap of $\epsilon_{PID}$ Matrix for an exclusive Cut'